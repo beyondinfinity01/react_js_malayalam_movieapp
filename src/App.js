2249092b5aed1c587067fc28e52de59099c286c8
Sajan Thomas <sajanthomas93@gmail.com>
2019-09-01 18:31:01 +0530

    movie app initial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MovieApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieList: [],
+      movieName: ''
+    }
+  }
+
+  saveMovieName = (e) => {
+    this.setState({ movieName: e.target.value })
+  }
+
+  saveMovie = (e) => {
+    e.preventDefault();
+    const movie = {
+      name: this.state.movieName,
+      id: +new Date()
+    }
+    this.setState({ movieList: this.state.movieList.concat(movie), movieName: '' });
+
+  }
+  render() {
+    return (
+      <div className="row container">
+        <form onSubmit={(e) => this.saveMovie(e)}>
+          <h4>{this.state.movieName}</h4>
+          <div className="row">
+            <div className="col s9">
+              <input value={this.state.movieName} onChange={(e) => { this.saveMovieName(e) }} type="text" />
+            </div>
+            <div className="col s3">
+              <button className="btn" type="submit">Save</button>
+            </div>
+
+          </div>
+        </form>
+        <ul className="collection">
+          {this.state.movieList.map((item) => {
+            return <li className="collection-item" key={item.id}>{item.name}</li>
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MovieApp;
